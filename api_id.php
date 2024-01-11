f7ac62c52a1611af5b4f8de59b9744ad43093d1a
Dionin <dondionin18@gmail.com>
2024-01-11 02:26:39 -0300

    API
    implementada la api de forma funciona

---
+++ api_id.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+  <h1 style="text-align: center;">no se que poner</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+            <form action="api.php" method="POST" enctype="multipart/form-data">
+
+                <label for="id_" class="form-label">Ingresa un numero (1 a 83):</label>
+                <input type="number" min="1" max="83" name="id_" id="id_" class="form-control" required>
+
+                <br>
+
+                <input type="submit" value="Hacer consulta" class="btn btn-outline-secondary">
+            </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
